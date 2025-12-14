3c12fd111531b5f5412797b7f0ebdd3e2e37e0e7
Adam Nguyen <kingofnightcore3@gmail.com>
2025-12-14 20:39:00 +0700

    Deploy static site with BASE_PATH=/AboutMe

---
+++ _next/static/chunks/bdce13df8caf6b53.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/6a1a87668344c978.js",
+  "static/chunks/6aa397b0da38cd0f.js",
+  "static/chunks/7f44547791aa1b5e.js",
+  "static/chunks/turbopack-9a1cf55068bfba79.js"
+])
